bd38a3c5bf683047e4db4bc6209ffdaed2e3e581
dan <mlekopan17@gmail.com>
2023-01-04 22:53:03 +0000

    fixed commands, replaced default website with custom

--- install-apache.sh
+++ install-apache.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 sudo apt update
+
+#installing apache2
 sudo apt install -y apache2
+
+#removing default website
+sudo rm -rf /var/www/html
+
+#copying website from host
+cp -r /vagrant/html/ /var/www/
+
+#start the apache service and add it to autostart
 sudo systemctl start apache2
 sudo systemctl enable apache2
