2351618e51b99fccb7def0729341ada075621bfe
dan <mlekopan17@gmail.com>
2023-03-14 23:29:26 +0000

    added script to replace ports, restarted apache service

--- install-apache.sh
+++ install-apache.sh
@@ -10,6 +10,11 @@ sudo rm -rf /var/www/html
 #copying website from host
 cp -r /vagrant/html/ /var/www/
 
+#replace the listening port
+sudo sed -i 's/80/8081/g' /etc/apache2/ports.conf
+sudo sed -i 's/80/8081/g' /etc/apache2/sites-enabled/000-default.conf
+
 #start the apache service and add it to autostart
+sudo systemctl stop apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
